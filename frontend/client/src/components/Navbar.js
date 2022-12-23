e37acca40cf0dbfffb7045604ca6a900fcac76aa
Rajesh25B <basvoju.rajesh@gmail.com>
2022-12-23 17:28:06 +0530

    implement navbar

--- frontend/client/src/components/Navbar.js
+++ frontend/client/src/components/Navbar.js
@@ -1,5 +1,62 @@
+import { Link, NavLink } from "react-router-dom";
+
 function Navbar() {
-  return <>Navbar</>;
+  const authLinks = (
+    <>
+      <li className="nav-item">
+        <NavLink className="nav-link" to="/dashboard">
+          Dashboard
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink className="nav-link" to="#!">
+          Logout
+        </NavLink>
+      </li>
+    </>
+  );
+
+  const guestLinks = (
+    <>
+      <li className="nav-item">
+        <NavLink className="nav-link" to="/login">
+          Login
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink className="nav-link" to="/register">
+          Register
+        </NavLink>
+      </li>
+    </>
+  );
+
+  return (
+    <nav className="navbar navbar-expand-lg bg-light">
+      <div className="container-fluid">
+        <Link className="navbar-brand">MomsStore</Link>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            {authLinks}
+            {guestLinks}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
