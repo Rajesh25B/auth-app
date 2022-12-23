4c0f4e2913009a8aa77a7adad93140ef1fb80d08
Rajesh25B <basvoju.rajesh@gmail.com>
2022-12-23 13:36:25 +0530

    setup react routing

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,5 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Dashboard from "components/Dashboard";
+import Home from "components/Home";
+import Login from "components/Login";
+import Register from "components/Register";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
